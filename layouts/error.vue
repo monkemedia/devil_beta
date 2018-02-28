c9bcb3a2cb22be39905d90adebeea8daf80b1a05
monkemedia <info@monkemedia.co.uk>
2018-02-28 23:00:32 +0000

    Quick changes to error page

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,17 +1,16 @@
 <template lang="pug">
-  .section
-    .container
-      .columns
-        .column.has-text-centered(v-if="error.statusCode === 404")
-          h1 {{ error.statusCode }}
-          p {{ error.message }}
-          nuxt-link.button.is-primary.is-flip(:to="error.path ? error.path : '/'")
-            span(data-text="Go Back") Go Back
-        .column.has-text-centered(v-else)
-          h1 {{ error.statusCode }}
-          p {{ error.message }}
-          nuxt-link.button.is-primary.is-flip(:to="error.path ? error.path : '/'")
-            span(data-text="Go Back") Go Back
+  .container
+    .columns
+      .column.has-text-centered(v-if="error.statusCode === 404")
+        h1 {{ error.statusCode }}
+        p {{ error.message }}
+        nuxt-link.button.is-primary.is-flip(:to="error.path ? error.path : '/'")
+          span(data-text="Go Back") Go Back
+      .column.has-text-centered(v-else)
+        h1 {{ error.statusCode }}
+        p {{ error.message }}
+        nuxt-link.button.is-primary.is-flip(:to="error.path ? error.path : '/'")
+          span(data-text="Go Back") Go Back
 </template>
 
 <script>
