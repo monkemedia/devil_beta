e6cd01b0b9b74846ec391cb93ab1c6f08c050b6a
monkemedia <info@monkemedia.co.uk>
2018-03-01 22:41:05 +0000

    Implemented dotenv config

--- server/index.js
+++ server/index.js
@@ -2,8 +2,8 @@ require('dotenv').config()
 const express = require('express')
 const { Nuxt, Builder } = require('nuxt')
 const app = express()
-const host = process.env.HOST || '127.0.0.1'
-const port = process.env.PORT || 3000
+const host = process.env.HOST
+const port = process.env.PORT
 
 app.set('port', port)
 
