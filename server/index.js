71651a17cc260ea5c8c08b0851cc9b7413b6ebe0
monkemedia <info@monkemedia.co.uk>
2018-03-02 10:34:19 +0000

    4

--- server/index.js
+++ server/index.js
@@ -2,7 +2,6 @@ require('dotenv').config()
 const express = require('express')
 const { Nuxt, Builder } = require('nuxt')
 const app = express()
-const wwwhisper = require('connect-wwwhisper')
 const host = process.env.HOST || '127.0.0.1'
 const port = process.env.PORT || 3000
 
