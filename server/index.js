6e7b3d4ffd65d122e42e76c1ee3645172344da9f
monkemedia <info@monkemedia.co.uk>
2018-03-02 09:53:46 +0000

    Added some express code to bypass heroku PORT error

--- server/index.js
+++ server/index.js
@@ -1,11 +1,6 @@
 const express = require('express')
 const { Nuxt, Builder } = require('nuxt')
 const app = express()
-
-// if (process.env.NODE_ENV === 'development') {
-//   require('dotenv').config()
-// }
-
 const host = process.env.HOST || '127.0.0.1'
 const port = process.env.PORT || 3000
 
@@ -15,16 +10,9 @@ app.set('port', port)
 let config = require('../nuxt.config.js')
 config.dev = !(process.env.NODE_ENV === 'production')
 
-// const isProduction = process.env.NODE_ENV === 'production'
-
 async function start() {
   // Init Nuxt.js
   const nuxt = new Nuxt(config)
-
-  // if (!isProduction) {
-  //   const wwwhisper = require('connect-wwwhisper')
-  //   app.use(wwwhisper())
-  // }
 
   // Build only in dev mode
   if (config.dev) {
