1521111e6348d9807cc07f2e9cbbfa3fffe0da69
monkemedia <info@monkemedia.co.uk>
2018-03-02 22:15:56 +0000

    Reintroduced searchbar back to header

--- store/index.js
+++ store/index.js
@@ -2,13 +2,15 @@ import Vuex         from 'vuex'
 import auth         from './modules/auth.js'
 import sellersItems from './modules/sellers-items.js'
 import cart         from './modules/cart.js'
+import modals       from './modules/modals.js'
 
 const createStore = () => {
   return new Vuex.Store({
     modules: {
       auth,
       sellersItems,
-      cart
+      cart,
+      modals
     }
   })
 }
