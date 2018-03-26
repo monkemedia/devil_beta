b2b10f9a363ec178a9bce60b41e8e41eb732080a
monkemedia <info@monkemedia.co.uk>
2018-03-26 17:18:27 +0100

    Cart sessions are transferred from ANON to official user on login

--- store/modules/auth.js
+++ store/modules/auth.js
@@ -70,7 +70,85 @@ const store = {
       return this.$axios.$put(`${process.env.BASE_URL}/users/${userId}.json?auth=${token}`, userDetails)
     },
 
-    loginUser ({ commit, getters, rootState, rootGetters }, authData) {
+    transferAnonCartToSignedInCart ({ getters, rootGetters }) {
+      const vm = this
+
+      function anonCartSessions (token, cartId) {
+        return vm.$axios.$get(`${process.env.BASE_URL}/cartSessions/${cartId}.json?auth=${token}`)
+      }
+
+      function deleteAnonCartSessions (token, cartId) {
+        return vm.$axios.$delete(`${process.env.BASE_URL}/cartSessions/${cartId}.json?auth=${token}`)
+      }
+
+      function addUidToCart (userId, cartId) {
+        return vm.$axios.$patch(`${process.env.BASE_URL}/users/${userId}.json?auth=${token}`, { cart: cartId })
+      }
+
+      function doesCartSessionExistInUserProfile (userId, token) {
+        return vm.$axios.$get(`${process.env.BASE_URL}/users/${userId}/cart.json?&auth=${token}`)
+      }
+
+      function addItemToCartSessions (token, cartId, items) {
+        return vm.$axios.$patch(`${process.env.BASE_URL}/cartSessions/${cartId}.json?auth=${token}`, items)
+      }
+      // Add anon cart uid to userprofile
+      const anonToken = rootGetters['cart/anonToken']
+      let userId
+      let token
+      let cartId
+
+      // When the user signs in Officially, let's see it they have a ANON token
+      console.log('User has signed in officially, lets see if they a ANON token')
+      if (anonToken) {
+        userId = getters['userId']
+        token = getters['token']
+        cartId = rootGetters['cart/anonUid']
+        // User has ANON token, let's see if they have a cart session
+        console.log('User has signed in officially, and they do have a anon token')
+        return doesCartSessionExistInUserProfile(userId, token)
+          .then((sessionId) => {
+            console.log('sessionId PEOPLE', sessionId)
+            if (!sessionId) {
+              console.log('Officially signed in user does not have a sessionCart Id ib user profile')
+              console.log('So add ANON cart session ID to user profile')
+              return addUidToCart(userId, cartId)
+                .then(() => {
+                  // Log out ANON user
+                  console.log('LOGOUT OUT ANON USER')
+                  return logoutAnonymousUser()
+                })
+            }
+    
+            // User has cart session ID so add anon session products to it
+            console.log('User has cart session ID so get anon session details then add it to offical cartSessions')
+            token = rootGetters['cart/anonToken']
+            return anonCartSessions(token, cartId)
+              .then((items) => {
+                token = getters['token']
+                return addItemToCartSessions(token, sessionId, items)
+              })
+              .then(() => {
+                // Remove ANON cart session
+                token = rootGetters['cart/anonToken']
+                return deleteAnonCartSessions(token, cartId)
+              })
+          })
+      }
+      // User doesnt have a ANON token so they don't have cart data saved anonymously, so don't do anything
+      console.log('User doesnt have a ANON token so they dont have cart data saved anonymously, so dont do anything')
+      return
+    },
+
+    deleteUser ({}, token) {
+      const authUrl = `https://www.googleapis.com/identitytoolkit/v3/relyingparty/deleteAccount?key=${process.env.FB_API_KEY}`
+
+      return this.$axios.$post(authUrl, {
+        idToken: token
+      })
+    },
+
+    loginUser ({ commit, getters, dispatch, rootState, rootGetters }, authData) {
       const authUrl = `https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=${process.env.FB_API_KEY}`
 
       function logoutAnonymousUser () {
@@ -110,61 +188,17 @@ const store = {
           return result
         })
         .then(() => {
-          const vm = this
-          function doesCartSessionExistInUserProfile (userId, cartId, token) {
-            return vm.$axios.$get(`${process.env.BASE_URL}/users/${userId}/cartIds.json?&auth=${token}`)
-          }
-          // Add anon cart uid to userprofile
-          const anonToken = rootGetters['cart/anonToken']
-          let userId
-          let token
-          let cartId
-
-          // When the user signs in Officially, let's see it they have a ANON token
-          console.log('User has signed in officially, lets see if they a ANON token')
-          if (anonToken) {
-            userId = getters['userId']
-            token = getters['token']
-            cartId = rootGetters['cart/anonUid']
-            // User has ANON token, let's see if they have a cart session
-            console.log('User has signed in officially, and they do have a anon token')
-            return doesCartSessionExistInUserProfile(userId, cartId, token)
-              .then((result) => {
-                if (!result) {
-                  console.log('Officially signed in user does not have a existing ANON cart session stored in thier user profile')
-                  console.log('So add ANON cart session with cart ID')
-                  return this.$axios.$post(`${process.env.BASE_URL}/users/${userId}/cartIds.json?auth=${token}`, { cartId })
-                    .then(() => {
-                      // Log out ANON user
-                      console.log('LOGOUT OUT ANON USER')
-                      return logoutAnonymousUser()
-                    })
-                }
-        
-                // User has cart session ID in user profile, so let's see if it contain the same product ID as
-                // the one store anonymously
-                console.log('User has cart session ID in user profile, so lets see if it contain the same product ID as the one store anonymously')
-                const filterCartIds = _.filter(result, (key) => {
-                  return key.cartId === cartId
-                })
-                // If it does contain the same product ID, then just logout out ANON user details
-                if (filterCartIds.length) {
-                  console.log('It does contain the same product ID, so dont do anything')
-                  return logoutAnonymousUser()
-                }
-                // If it doesnt contain the same product ID, then add the card ID to user profile 
-                console.log('It doesnt contain the same product ID, so add the cart ID to user profile')
-                return this.$axios.$post(`${process.env.BASE_URL}/users/${userId}/cartIds.json?auth=${token}`, { cartId })
-                  .then(() => {
-                    // Log out ANON user
-                    console.log('LOGOUT OUT ANON USER')
-                    return logoutAnonymousUser()
-                  })
-              })
-          }
-          // User doesnt have a ANON token so they don't have cart data saved anonymously, so don't do anything
-          console.log('User doesnt have a ANON token so they dont have cart data saved anonymously, so dont do anything')
-          return
+          return dispatch('transferAnonCartToSignedInCart')
+        })
+        .then(() => {
+          // Delete anon user
+          const token = rootGetters['cart/anonToken']
+          return dispatch('deleteUser', token)
+        })
+        .then(() => {
+          // then Log out ANON user
+          console.log('LOGOUT OUT ANON USER')
+          return logoutAnonymousUser()
         })
         .catch((err) => {
           console.log(err)
