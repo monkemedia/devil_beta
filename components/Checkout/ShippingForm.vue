a27c0f50a6090791bb6da0aec4280f7e9f37238c
monkemedia <info@monkemedia.co.uk>
2018-04-06 15:55:00 +0100

    created shipping page and shipping form component

---
+++ components/Checkout/ShippingForm.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+  .card
+    header.card-header
+      .card-header-title
+        p 1. Shipping
+    .card-content
+      .content
+        p test
+</template>
+
+<script>
+  export default {
+    name: 'ShippingForm'
+  }
+</script>
+
+<style lang="stylus" scoped>
+  @import '~assets/css/utilities/variables.styl'
+
+</style>
--- pages/shipping/index.vue
+++ pages/shipping/index.vue
@@ -1,10 +1,27 @@
 <template lang="pug">
-  p shipping page
+  .container.default-container-padding.checkout
+    .columns
+      .column
+        h1 Checkout
+    .columns
+      .column.is-12-tablet.is-8-widescreen
+        shipping-form
+      .column.is-12-tablet.is-4-widescreen
+        order-summary
 
 </template>
 
 <script>
+  import ShippingForm from '@/components/Checkout/ShippingForm'
+  import OrderSummary from '@/components/Checkout/OrderSummary'
+
   export default {
+    name: 'Shipping',
+
+    components: {
+      ShippingForm,
+      OrderSummary
+    }
   }
 </script>
 
